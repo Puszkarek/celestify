72c265550617fdb48acc57c8e1cd4c83b83e40f9
puszkarek <guipuszkarek@gmail.com>
2023-05-13 00:10:00 -0300

    🔧 Update assets reference

--- src/constants/poster.ts
+++ src/constants/poster.ts
@@ -1,25 +1,27 @@
-import { GalaxyStarCount } from '@app/interfaces/galaxy';
-import { PosterItemSize } from '@app/interfaces/poster';
+import { CelestialBody, GalaxyStarCount } from '@app/interfaces/galaxy';
+import { GridItemSize } from '@app/interfaces/poster';
 
 export const POSTER_CELESTIAL_BODY_SIZES: [
-  PosterItemSize,
-  ...Array<PosterItemSize>,
+  GridItemSize,
+  ...Array<GridItemSize>,
 ] = [
   { width: 5, height: 6 },
   { width: 6, height: 7 },
   { width: 7, height: 8 },
 ];
 
-export const CELESTIAL_BODY_TYPES_COUNT = {
-  'gas-giant': [0, 1],
-  'oceanic': [0, 1],
-  'black-hole': [0, 1],
-  'electric': [0, 1],
-  'ice': [0, 1],
-  'rocky': [0, 1],
-  'supernova': [0, 1],
-  'vulcanic': [0, 1],
-  'wildlife': [0, 1],
+export const CELESTIAL_BODY_TYPES_COUNT: Record<
+  CelestialBody['type'],
+  Array<number>
+> = {
+  'black-hole': [0, 1, 2, 3, 4, 5],
+  'electric': [0, 1, 2, 3, 4],
+  'gas-giant': [0, 1, 2, 3, 4, 5, 6],
+  'oceanic': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  'rocky': [0, 1, 2, 3, 4, 5, 6, 7],
+  'star': [0, 1, 2, 3, 4, 5, 6],
+  'vulcanic': [0, 1, 2, 3, 4, 5, 6, 7],
+  'wildlife': [0, 1, 2, 3, 4, 5, 6],
 };
 
 export const STAR_TYPES_COUNT: [GalaxyStarCount, ...Array<GalaxyStarCount>] = [
